b6e112a54bf55614d72aac0f84b59e4004067200
Tyson Clugg <tyson@clugg.net>
2017-07-24 19:19:28 +1000

    Rework python route receiver.

--- api-notification.py
+++ api-notification.py
@@ -1,25 +1,78 @@
 #!/usr/bin/env python
-
-from pprint import pprint
+from __future__ import print_function, unicode_literals
+import datetime
+import fileinput
+import json
 import os
 import sys
-import time
-sys.stderr.write(os.getcwd())
-sys.stderr.flush()
 
-logfile = open(os.path.join(os.path.dirname(__file__), 'mw.log'), 'a+')
+
+PID = os.getpid()
+LOG = open(os.path.join(os.path.dirname(__file__), 'mw.log'), 'a+')
+
 
 def log(data):
-    pprint(line, logfile)
-    logfile.flush()
+    LOG.write(
+        '{ts} [{pid}] {data!r}\n'.format(
+            ts=datetime.datetime.now().isoformat(),
+            pid=PID,
+            data={str(key): val for key, val in data.items()} if isinstance(data, dict) else data,
+        )
+    )
+    LOG.flush()
 
-try:
-    now = time.time()
-    while True and time.time() < now + 5:
-        line = sys.stdin.readline().strip()
-        log(line)
-        if not line or 'shutdown' in line:
-            break
-        time.sleep(1)
-except IOError:
-    pass
+
+print('version')
+
+
+def on_update(address, asn, direction, message, **kwargs):
+    """
+    Update a route advertisement.
+    """
+    if direction != 'receive':
+        return  # only interested in received routes
+
+    update = message.get('update', None)
+    if update is None:
+        return  # not a route update
+
+    attribute = update['attribute']
+    as_path = attribute['as-path']
+
+    for source, announcements in update.get('announce', {}).items():
+        for nexthop, routes in announcements.items():
+            for route in routes:
+                for route_type, subnet in route.items():
+                    network, prefix = subnet.split('/')
+                    if not network.startswith('10.10.'):
+                        continue  # not a MW route
+                    if not prefix.isdigit():
+                        continue  # invalid prefix
+                    prefix = int(prefix)
+                    if not 24 <= prefix <= 32:
+                        continue  # invalid prefix length
+                    print(
+                        'Received route to {subnet} via {as_path} ({nexthop}) from [{peer_asn}] ({peer_address})'.format(
+                            peer_asn=asn['peer'],
+                            peer_address=address['peer'],
+                            nexthop=nexthop,
+                            subnet=subnet,
+                            as_path=as_path,
+                        ),
+                        file=sys.stderr,
+                    )
+
+
+for line in fileinput.input():
+    log(line)
+    try:
+        data = json.loads(line)
+        msg = data.get('type', None)
+        if msg == 'update':
+            on_update(**data['neighbor'])
+        else:
+            log(data)
+            if data.get('notification') == 'shutdown':
+                break
+    except Exception as err:
+        log(err)
